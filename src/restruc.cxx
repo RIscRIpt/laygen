40eec4607d6d4c9fb35f2201aa55cb753cdc08d6
RIscRIpt <richard@riscript.com>
2020-03-15 22:06:03 +0200

    Refactor Restruc, improve analysis

--- src/restruc.cxx
+++ src/restruc.cxx
@@ -4,6 +4,7 @@
 
 #include <cinttypes>
 #include <cstdio>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -53,157 +54,298 @@ static bool is_conditional_jump(ZydisMnemonic mnemonic)
     return false;
 }
 
-static void print_instruction(ZydisDecodedInstruction const &instruction)
+void Restruc::Flow::merge(Flow &other)
 {
-    char buffer[256];
-    ZydisFormatter formatter;
-    ZYAN_THROW(ZydisFormatterInit(&formatter, ZYDIS_FORMATTER_STYLE_INTEL));
-    ZYAN_THROW(ZydisFormatterFormatInstruction(&formatter,
-                                               &instruction,
-                                               buffer,
-                                               sizeof(buffer),
-                                               0));
-    std::cout << buffer << '\n';
+    instructions.merge(other.instructions);
+    impl::merge_keeping_src_unique(inner_jumps, other.inner_jumps);
+    impl::merge_keeping_src_unique(outer_jumps, other.outer_jumps);
+    impl::merge_keeping_src_unique(unknown_jumps, other.unknown_jumps);
+    impl::merge_keeping_src_unique(calls, other.calls);
 }
 
-Restruc::Function::Function()
+Restruc::CFGraph::CFGraph()
     : entry_point(nullptr)
+    , outer_cfgraph(nullptr)
 {
 }
 
-Restruc::Function::Function(ZydisDecoder const &decoder,
-                            Address entry_point,
-                            Address end)
+Restruc::CFGraph::CFGraph(Address entry_point, CFGraph *outer_cfgraph)
     : entry_point(entry_point)
+    , outer_cfgraph(outer_cfgraph)
 {
-    // TODO: analyze bounds
-    Address address = entry_point;
-    while (address != nullptr) {
-        ZydisDecodedInstruction instruction;
-        ZYAN_THROW(ZydisDecoderDecodeBuffer(&decoder,
-                                            address,
-                                            ZYDIS_MAX_INSTRUCTION_LENGTH,
-                                            &instruction));
-#ifndef NDEBUG
-        print_instruction(instruction);
-#endif
-        visit(address);
-        instructions_.emplace(address, instruction);
-        Address next_address = address + instruction.length;
-        if (instruction.mnemonic == ZYDIS_MNEMONIC_CALL) {
-            // Assume calls always return (i.e. they are not no-return)
-            Address dst = next_address + instruction.operands[0].imm.value.s;
-            add_call(dst, address, next_address);
+}
+
+bool Restruc::CFGraph::is_complete() const
+{
+    return !instructions.empty() && unknown_jumps.empty() && has_ret;
+}
+
+bool Restruc::CFGraph::can_merge_with_outer_cfgraph() const
+{
+    if (!outer_cfgraph) {
+        return false;
+    }
+    if (is_complete()) {
+        return true;
+    }
+    if (instructions.empty()) {
+        return false;
+    }
+    auto const &last_outer_instruction = *outer_cfgraph->instructions.rbegin();
+    auto const &first_instruction = *instructions.begin();
+    // Can merge if first instruction of this CFGraph is comes right
+    // after the last instruction of outer CFGraph.
+    return first_instruction.first
+           == last_outer_instruction.first
+                  + last_outer_instruction.second.length;
+}
+
+Address Restruc::CFGraph::analyze(Address address)
+{
+    auto const &instruction = instructions[address];
+    Address next_address = address + instruction.length;
+    visit(address);
+    if (instruction.mnemonic == ZYDIS_MNEMONIC_CALL) {
+        // Assume calls always return (i.e. they are not no-return)
+        Address dst = next_address + instruction.operands[0].imm.value.s;
+        add_call(dst, address, next_address);
+    }
+    else if (instruction.mnemonic == ZYDIS_MNEMONIC_RET) {
+        has_ret = true;
+        if (!is_inside(next_address)) {
+            next_address = nullptr;
         }
-        else if (instruction.mnemonic == ZYDIS_MNEMONIC_RET) {
-            if (!is_inside_function(next_address)) {
-                break;
-            }
-        }
-        else if (instruction.mnemonic == ZYDIS_MNEMONIC_JMP
-                 || is_conditional_jump(instruction.mnemonic)) {
-            bool unconditional = instruction.mnemonic == ZYDIS_MNEMONIC_JMP;
-            Address dst = next_address + instruction.operands[0].imm.value.s;
-            auto type = get_jump_type(dst, address, next_address);
-            add_jump(type, dst, address);
-            if (unconditional) {
-                if (type == Jump::Inner) {
-                    next_address = dst;
+    }
+    else if (instruction.mnemonic == ZYDIS_MNEMONIC_JMP
+             || is_conditional_jump(instruction.mnemonic)) {
+        bool unconditional = instruction.mnemonic == ZYDIS_MNEMONIC_JMP;
+        auto offset = instruction.operands[0].imm.value.s;
+        Address dst = next_address + offset;
+        auto type = get_jump_type(dst, address, next_address);
+        add_jump(type, dst, address);
+        if (unconditional) {
+            switch (type) {
+            case Jump::Unknown:
+                if (promote_unknown_jump(next_address, Jump::Inner)) {
+                    return next_address;
                 }
                 else {
-                    break;
+                    return nullptr;
                 }
-            }
-            else {
-                unvisited_.emplace(dst);
+                break;
+            case Jump::Inner:
+                if (dst >= next_address) {
+                    return next_address;
+                }
+                else {
+                    // Looping inside CFGraph
+                    return nullptr;
+                }
+            case Jump::Outer:
+                //
+                return nullptr;
             }
         }
-        promote_unknown_to_inner_jump(next_address);
-        address = next_address;
     }
+    return next_address;
 }
 
-void Restruc::Function::promote_unknown_to_inner_jump(Address dst)
+void Restruc::CFGraph::add_instruction(
+    Address address,
+    ZydisDecodedInstruction const &instruction)
 {
-    if (auto jump = unknown_jumps_.find(dst); jump != unknown_jumps_.end()) {
-        add_jump(Jump::Inner, dst, jump->second.src);
-        unknown_jumps_.erase(jump);
-    }
+    instructions.emplace(address, instruction);
 }
 
-void Restruc::Function::visit(Address address)
-{
-    if (auto uv = unvisited_.find(address); uv != unvisited_.end()) {
-        unvisited_.erase(uv);
-    }
-}
-
-Restruc::Jump::Type
-Restruc::Function::get_jump_type(Address dst, Address src, Address next_address)
-{
-    // If jumping with offset 0, i.e. no jump
-    if (dst == next_address) {
-        return Jump::Inner;
-    }
-    // If jump is first function instruction
-    if (instructions_.size() == 1) {
-        return Jump::Outer;
-    }
-    if (instructions_.find(dst) != instructions_.end()) {
-        return Jump::Inner;
-    }
-    // If jumping above function entry-point
-    if (instructions_.begin()->first > dst) {
-        return Jump::Outer;
-    }
-    // TODO: handle more cases
-    return Jump::Unknown;
-}
-
-void Restruc::Function::add_jump(Jump::Type type, Address dst, Address src)
+void Restruc::CFGraph::add_jump(Jump::Type type, Address dst, Address src)
 {
     switch (type) {
     case Jump::Inner:
-        inner_jumps_.emplace(dst, Jump(Jump::Inner, dst, src));
+        inner_jumps.emplace(dst, Jump(Jump::Inner, dst, src));
         break;
     case Jump::Outer:
-        outer_jumps_.emplace(dst, Jump(Jump::Outer, dst, src));
+        outer_jumps.emplace(dst, Jump(Jump::Outer, dst, src));
         break;
     case Jump::Unknown:
-        unknown_jumps_.emplace(dst, Jump(Jump::Unknown, dst, src));
+        unknown_jumps.emplace(dst, Jump(Jump::Unknown, dst, src));
         break;
     }
 }
 
-void Restruc::Function::add_call(Address dst, Address src, Address ret)
+void Restruc::CFGraph::add_call(Address dst, Address src, Address ret)
 {
-    calls_.emplace(src, Call(dst, src, ret));
+    calls.emplace(src, Call(dst, src, ret));
 }
 
-bool Restruc::Function::is_inside_function(Address address)
+bool Restruc::CFGraph::promote_unknown_jump(Address dst, Jump::Type new_type)
 {
-    return instructions_.find(address) != instructions_.end()
-           || inner_jumps_.find(address) != inner_jumps_.end();
+    bool promoted = false;
+    while (true) {
+        if (auto jump = unknown_jumps.extract(dst); !jump.empty()) {
+            promoted = true;
+            add_jump(new_type, dst, jump.mapped().src);
+        }
+        else {
+            break;
+        }
+    }
+    return promoted;
+}
+
+bool Restruc::CFGraph::promote_outer_unknown_jump(Address dst,
+                                                  Jump::Type new_type)
+{
+    if (!outer_cfgraph) {
+        return false;
+    }
+    return outer_cfgraph->promote_unknown_jump(dst, new_type);
+}
+
+void Restruc::CFGraph::visit(Address address)
+{
+    promote_unknown_jump(address, Jump::Inner);
+    promote_outer_unknown_jump(address, Jump::Inner);
+}
+
+Restruc::Jump::Type
+Restruc::CFGraph::get_jump_type(Address dst, Address src, Address next)
+{
+    // If jumping with offset 0, i.e. no jump
+    if (dst == next) {
+        return Jump::Inner;
+    }
+    // If jump is first function instruction
+    if (instructions.size() == 1) {
+        return Jump::Outer;
+    }
+    // If destination is one of the previous instructions
+    if (instructions.find(dst) != instructions.end()) {
+        return Jump::Inner;
+    }
+    // If jumping above entry-point
+    if (dst < entry_point) {
+        return Jump::Outer;
+    }
+    return Jump::Unknown;
+}
+
+bool Restruc::CFGraph::is_inside(Address address)
+{
+    return instructions.find(address) != instructions.end()
+           || inner_jumps.find(address) != inner_jumps.end();
 }
 
 Restruc::Restruc(std::filesystem::path const &pe_path)
     : pe_(pe_path)
 {
+    ZYAN_THROW(ZydisDecoderInit(&decoder_,
+                                ZYDIS_MACHINE_MODE_LONG_64,
+                                ZYDIS_ADDRESS_WIDTH_64));
+#ifndef NDEBUG
+    ZYAN_THROW(ZydisFormatterInit(&formatter_, ZYDIS_FORMATTER_STYLE_INTEL));
+#endif
 }
 
-void Restruc::safe_add_function(ZydisDecoder const &decoder,
-                                Address address,
-                                Address end)
+void Restruc::fill_cfgraph(CFGraph &cfgraph)
 {
-    // Prevent recursive analysis
-    if (functions_.find(address) != functions_.end()) {
+    Address address;
+    Address next_address;
+    if (cfgraph.instructions.empty()) {
+        next_address = cfgraph.entry_point;
+    }
+    else {
+        next_address = cfgraph.instructions.rbegin()->first;
+    }
+    Address end = pe_.get_end(next_address);
+    while (true) {
+        address = next_address;
+        if (address == nullptr || address >= end) {
+            break;
+        }
+
+        ZydisDecodedInstruction instruction;
+        ZYAN_THROW(ZydisDecoderDecodeBuffer(&decoder_,
+                                            address,
+                                            end - address,
+                                            &instruction));
+#ifndef NDEBUG
+        dump_instruction(std::clog,
+                         pe_.raw_to_virtual_address(address),
+                         instruction);
+#endif
+
+        cfgraph.add_instruction(address, instruction);
+        next_address = cfgraph.analyze(address);
+    }
+}
+
+void Restruc::resolve_incomplete_cfgraph(CFGraph &outer_cfgraph)
+{
+    if (outer_cfgraph.instructions.empty()
+        || outer_cfgraph.unknown_jumps.empty()) {
         return;
     }
-    functions_.emplace(address, Function(decoder, address, end));
-    unanalyzed_functions_.push_back(address);
+    while (!outer_cfgraph.unknown_jumps.empty()) {
+        auto const unknown_jump_dst =
+            outer_cfgraph.unknown_jumps.begin()->first;
+        CFGraph new_cfgraph(unknown_jump_dst, &outer_cfgraph);
+        Address address;
+        Address next_address = new_cfgraph.entry_point;
+        Address end = pe_.get_end(new_cfgraph.entry_point);
+        bool can_merge = false;
+        while (!can_merge) {
+            address = next_address;
+            if (address == nullptr || address >= end) {
+                break;
+            }
+
+            ZydisDecodedInstruction instruction;
+            ZYAN_THROW(ZydisDecoderDecodeBuffer(&decoder_,
+                                                address,
+                                                end - address,
+                                                &instruction));
+#ifndef NDEBUG
+            dump_instruction(std::clog,
+                             pe_.raw_to_virtual_address(address),
+                             instruction);
+#endif
+
+            new_cfgraph.add_instruction(address, instruction);
+            next_address = new_cfgraph.analyze(address);
+            can_merge = new_cfgraph.can_merge_with_outer_cfgraph();
+        }
+        if (can_merge) {
+            outer_cfgraph.merge(new_cfgraph);
+            break;
+        }
+        else {
+            outer_cfgraph.promote_unknown_jump(unknown_jump_dst, Jump::Outer);
+        }
+    }
 }
 
-Restruc::Address Restruc::pop_unanalyzed_function()
+void Restruc::create_function(Address entry_point)
+{
+    // Prevent recursive analysis
+    if (functions_.find(entry_point) != functions_.end()) {
+        return;
+    }
+
+    Address address = entry_point;
+    CFGraph cfgraph(entry_point);
+    while (true) {
+        fill_cfgraph(cfgraph);
+        if (cfgraph.is_complete()) {
+            break;
+        }
+        resolve_incomplete_cfgraph(cfgraph);
+    }
+
+    functions_.emplace(entry_point, cfgraph);
+    unanalyzed_functions_.push_back(entry_point);
+}
+
+Address Restruc::pop_unanalyzed_function()
 {
     auto address = unanalyzed_functions_.front();
     unanalyzed_functions_.pop_front();
@@ -212,19 +354,63 @@ Restruc::Address Restruc::pop_unanalyzed_function()
 
 void Restruc::analyze()
 {
-    ZydisDecoder decoder;
-    ZYAN_THROW(ZydisDecoderInit(&decoder,
-                                ZYDIS_MACHINE_MODE_LONG_64,
-                                ZYDIS_ADDRESS_WIDTH_64));
-
-    safe_add_function(decoder, pe_.get_entry_point(), nullptr);
+    create_function(pe_.get_entry_point());
     while (!unanalyzed_functions_.empty()) {
         auto &function = functions_[pop_unanalyzed_function()];
-        for (auto [_, call] : function.get_calls()) {
-            safe_add_function(decoder, call.dst, nullptr);
+
+        // Iterate over unique call destinations
+        for (auto it = function.calls.begin(), end = function.calls.end();
+             it != end;
+             it = function.calls.upper_bound(it->first)) {
+            create_function(it->second.dst);
         }
-        for (auto [_, jump] : function.get_outer_jumps()) {
-            safe_add_function(decoder, jump.dst, nullptr);
+
+        // Iterate over unique outer jumps
+        for (auto it = function.outer_jumps.begin(),
+                  end = function.outer_jumps.end();
+             it != end;
+             it = function.outer_jumps.upper_bound(it->first)) {
+            create_function(it->second.dst);
         }
     }
 }
+
+#ifndef NDEBUG
+
+void Restruc::debug(std::ostream &os)
+{
+    auto address = pe_.get_entry_point();
+    create_function(address);
+    dump_function(os, formatter_, functions_[address]);
+}
+
+void Restruc::dump_instruction(std::ostream &os,
+                               DWORD va,
+                               ZydisDecodedInstruction const &instruction)
+{
+    char buffer[256];
+    ZYAN_THROW(ZydisFormatterFormatInstruction(&formatter_,
+                                               &instruction,
+                                               buffer,
+                                               sizeof(buffer),
+                                               va));
+    os << std::hex << std::setfill('0') << std::setw(8) << va << "    "
+       << buffer << '\n';
+}
+
+void Restruc::dump_function(std::ostream &os,
+                            ZydisFormatter const &formatter,
+                            CFGraph const &function)
+{
+    char buffer[256];
+    os << std::hex << std::setfill('0');
+    os << std::setw(8) << pe_.raw_to_virtual_address(function.entry_point)
+       << ":\n";
+    for (auto const &[address, instruction] : function.instructions) {
+        auto va = pe_.raw_to_virtual_address(address);
+        dump_instruction(os, va, instruction);
+    }
+    os << '\n';
+}
+
+#endif
--- src/restruc.hxx
+++ src/restruc.hxx
@@ -10,9 +10,11 @@
 
 namespace rstc {
 
+    using Address = BYTE *;
+
     class Restruc {
     public:
-        using Address = BYTE *;
+        using Instructions = std::map<Address, ZydisDecodedInstruction>;
 
         struct Jump {
             enum Type {
@@ -30,6 +32,7 @@ namespace rstc {
             Address const dst;
             Address const src;
         };
+        using Jumps = std::multimap<Address, Jump>;
 
         struct Call : public Jump {
             Call(Address dst, Address src, Address ret)
@@ -39,70 +42,115 @@ namespace rstc {
             }
             Address const ret;
         };
+        using Calls = std::multimap<Address, Call>;
 
-        class Function {
-        public:
-            using Instructions = std::map<Address, ZydisDecodedInstruction>;
+    private:
+        struct Flow {
+            Instructions instructions;
+            Jumps inner_jumps;
+            Jumps outer_jumps;
+            Jumps unknown_jumps;
+            Calls calls;
+            bool has_ret = false;
 
-            Function();
-            Function(ZydisDecoder const &decoder,
-                     Address entry_point,
-                     Address end);
+            void merge(Flow &other);
+        };
+
+        struct CFGraph : public Flow {
+            CFGraph();
+            CFGraph(Address entry_point, CFGraph *outer_cfgraph = nullptr);
 
             Address const entry_point;
+            CFGraph *const outer_cfgraph;
 
-            inline const Instructions get_instructions() const
-            {
-                return instructions_;
-            }
+            bool is_complete() const;
+            bool can_merge_with_outer_cfgraph() const;
+            Address analyze(Address address);
 
-            inline const std::map<Address, Jump> get_inner_jumps() const
-            {
-                return inner_jumps_;
-            }
-            inline const std::map<Address, Jump> get_outer_jumps() const
-            {
-                return outer_jumps_;
-            }
-            inline const std::map<Address, Call> get_calls() const
-            {
-                return calls_;
-            }
-
-            bool is_inside_function(Address address);
-
-        private:
-            void promote_unknown_to_inner_jump(Address dst);
-            void visit(Address address);
-
-            Jump::Type
-            get_jump_type(Address dst, Address src, Address next_address);
-
+            void add_instruction(Address address,
+                                 ZydisDecodedInstruction const &instruction);
             void add_jump(Jump::Type type, Address dst, Address src);
             void add_call(Address dst, Address src, Address ret);
 
-            Instructions instructions_;
-            std::map<Address, Jump> inner_jumps_;
-            std::map<Address, Jump> outer_jumps_;
-            std::map<Address, Jump> unknown_jumps_;
-            std::map<Address, Call> calls_;
-            std::set<Address> unvisited_;
+            void visit(Address address);
+            bool is_inside(Address address);
+            bool promote_unknown_jump(Address dst, Jump::Type new_type);
+            bool promote_outer_unknown_jump(Address dst, Jump::Type new_type);
+            Jump::Type get_jump_type(Address dst, Address src, Address next);
+        };
+
+    public:
+        class Function : private Flow {
+        public:
+            Function(Instructions &&instructions);
+
+            inline Jumps const &get_outer_jumps() const { return outer_jumps; }
         };
 
         Restruc(std::filesystem::path const &pe_path);
 
         void analyze();
 
+#ifndef NDEBUG
+        void debug(std::ostream &os);
+        void dump_instruction(std::ostream &os,
+                              DWORD va,
+                              ZydisDecodedInstruction const &instruction);
+        void dump_function(std::ostream &os,
+                           ZydisFormatter const &formatter,
+                           CFGraph const &function);
+#endif
+
     private:
-        void
-        safe_add_function(ZydisDecoder const &decoder, Address address, Address end);
+        void fill_cfgraph(CFGraph &cfgraph);
+        void resolve_incomplete_cfgraph(CFGraph &cfgraph);
+        void create_function(Address entry_point);
 
         Address pop_unanalyzed_function();
 
+        ZydisDecoder decoder_;
+#ifndef NDEBUG
+        ZydisFormatter formatter_;
+#endif
+
         PE pe_;
 
-        std::map<Address, Function> functions_;
+        std::map<Address, CFGraph> functions_;
         std::deque<Address> unanalyzed_functions_;
     };
+
+    namespace impl {
+        /*
+        // clang-format off
+        template<typename T>
+        concept MultimapOfDestinations = requires
+        {
+            { T::value_type::first_type } -> Address;
+            { T::value_type::second_type::dst } -> Address const;
+            { T::value_type::second_type::src } -> Address const;
+        };
+        // clang-format on
+        */
+
+        template</*MultimapOfDestinations*/ typename Map>
+        void merge_keeping_src_unique(Map &dst_map, Map &src_map)
+        {
+            for (auto const &[_, src] : src_map) {
+                bool dst_map_has_src = false;
+                for (auto ii = dst_map.equal_range(src.dst);
+                     ii.first != ii.second;
+                     ++ii.first) {
+                    auto dst_src = ii.first->second.src;
+                    if (dst_src == src.src) {
+                        dst_map_has_src = true;
+                        break;
+                    }
+                }
+                if (!dst_map_has_src) {
+                    dst_map.emplace(src.dst, src);
+                }
+            }
+        }
+    }
 
 }
