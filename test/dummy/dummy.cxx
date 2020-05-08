14641bac80bf463db9030e6b22e5d5dba9a36328
RIscRIpt <richard@riscript.com>
2020-05-08 20:05:47 +0300

    Add dummy

---
+++ test/dummy/dummy.cxx
@@ -0,0 +1,35 @@
+struct S {
+    int a[4];
+    char const *b;
+    float c;
+};
+
+long long hash(long long v)
+{
+    return v * 0x5851F42D4C957F2D + 0x14057B7EF767814F;
+}
+
+int test(S const *s)
+{
+    int result = 0;
+    for (int i = 0; i < 4; i++) {
+        result ^= hash(s->a[i]);
+        result ^= hash(s->b[i]);
+    }
+    result %= static_cast<int>(1.0f / s->c);
+    return result;
+}
+
+int main()
+{
+    S s;
+    char h[5];
+    for (int i = 0; i < 4; i++) {
+        s.a[i] = hash(i);
+        h[i] = 'A' + s.a[i] % ('Z' - 'A');
+    }
+    h[4] = '\0';
+    s.b = h;
+    s.c = 0.01;
+    return test(&s);
+}
