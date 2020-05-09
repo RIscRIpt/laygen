92e407db81e2113e3e771895042636b54315537e
RIscRIpt <richard@riscript.com>
2020-05-09 22:25:36 +0300

    Change dummy to use reference instead of pointer

--- test/dummy/dummy.cxx
+++ test/dummy/dummy.cxx
@@ -9,14 +9,14 @@ long long hash(long long v)
     return v * 0x5851F42D4C957F2D + 0x14057B7EF767814F;
 }
 
-int test(S const *s)
+int test(S const &s)
 {
     int result = 0;
     for (int i = 0; i < 4; i++) {
-        result ^= hash(s->a[i]);
-        result ^= hash(s->b[i]);
+        result ^= hash(s.a[i]);
+        result ^= hash(s.b[i]);
     }
-    result %= static_cast<int>(1.0f / s->c);
+    result %= static_cast<int>(1.0f / s.c);
     return result;
 }
 
@@ -31,5 +31,5 @@ int main()
     h[4] = '\0';
     s.b = h;
     s.c = 0.01;
-    return test(&s);
+    return test(s);
 }
