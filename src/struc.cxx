f4721e1d903a67dadbc24fbcc6dce19080224329
RIscRIpt <richard@riscript.com>
2020-06-04 00:15:25 +0300

    Implement Struc

--- src/struc.cxx
+++ src/struc.cxx
@@ -1,1 +1,68 @@
 #include "struc.hxx"
+
+#include "utils/adapters.hxx"
+
+#include <algorithm>
+#include <cassert>
+
+using namespace rstc;
+
+Struc::Field::Field(Type type,
+                    size_t size,
+                    size_t count,
+                    const class Struc *struc)
+    : struc_(struc)
+    , size_(size)
+    , count_(count)
+    , type_(type)
+{
+}
+
+void Struc::add_int_field(size_t offset, size_t size, size_t count)
+{
+    // Is 1, 2, 4, 8, 16, 32, 64
+    assert(size > 0 && (size & (size - 1)) == 0 && size <= 64);
+    fields_.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(offset),
+        std::forward_as_tuple(Field(Field::Int, size, count, Atomic)));
+}
+
+void Struc::add_float_field(size_t offset, size_t size, size_t count)
+{
+    assert(size >= 4 && (size & (size - 1)) == 0 && size <= 8);
+    fields_.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(offset),
+        std::forward_as_tuple(Field(Field::Float, size, count, Atomic)));
+}
+
+void Struc::add_pointer_field(size_t offset, size_t count, const Struc *struc)
+{
+    fields_.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(offset),
+        std::forward_as_tuple(Field(Field::Pointer, 8, count, struc)));
+}
+
+void Struc::add_struc_field(size_t offset, const Struc *struc, size_t count)
+{
+    fields_.emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(offset),
+        std::forward_as_tuple(Field(Field::Struc, 0, count, struc)));
+}
+
+size_t Struc::get_size() const
+{
+    if (fields_.empty()) {
+        return 0;
+    }
+    auto last_offset = fields_.rbegin()->first;
+    auto last_fields = utils::multimap_values(fields_.equal_range(last_offset));
+    auto largest_last_field = std::max_element(
+        last_fields.begin(),
+        last_fields.end(),
+        [](Field const &a, Field const &b) { return a.size() < b.size(); });
+    return last_offset + largest_last_field->size();
+}
--- src/struc.hxx
+++ src/struc.hxx
@@ -1,8 +1,61 @@
 #pragma once
 
+#include <map>
+
 namespace rstc {
 
     class Struc {
+    public:
+        class Field {
+        public:
+            friend class Struc;
+
+            enum Type {
+                Int,
+                Float,
+                Pointer,
+                Struc,
+            };
+
+            inline Type type() const { return type_; }
+            inline size_t size() const
+            {
+                return type_ != Struc ? size_ : struc_->get_size();
+            }
+            inline size_t count() const { return count_; }
+            inline const class Struc *struc() const { return struc_; }
+
+        private:
+            Field(Type type,
+                  size_t size,
+                  size_t count,
+                  const class Struc *struc);
+
+            const class Struc *struc_;
+            size_t size_;
+            size_t count_;
+            Type type_;
+        };
+
+        void add_int_field(size_t offset, size_t size, size_t count = 1);
+        void add_float_field(size_t offset, size_t size, size_t count = 1);
+        void add_pointer_field(size_t offset,
+                               size_t count = 1,
+                               const Struc *struc = nullptr);
+        void
+        add_struc_field(size_t offset, const Struc *struc, size_t count = 1);
+
+        size_t get_size() const;
+
+        inline std::multimap<size_t, Field> const &fields() const
+        {
+            return fields_;
+        }
+
+        static constexpr const Struc *const Atomic = nullptr;
+
+    private:
+        std::multimap<size_t, Field> fields_;
     };
 
 }
