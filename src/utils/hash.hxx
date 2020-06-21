23b1f5cabc4b160bf240ded2756e504f7d44ed7a
RIscRIpt <richard@riscript.com>
2020-06-21 15:05:28 +0300

    Improve utils::hash_combine, add utils::hash_reverse

--- src/utils/hash.hxx
+++ src/utils/hash.hxx
@@ -2,13 +2,24 @@
 
 #include <functional>
 
-namespace rstc::utils {
+namespace rstc::utils::hash {
 
     template<class T>
-    static inline void hash_combine(std::size_t &seed, const T &v)
+    static inline void combine(std::size_t &seed, const T &v)
     {
         std::hash<T> hasher;
-        seed ^= hasher(v) * 0x5851F42D4C957F2D + 0x14057B7EF767814F;
+        seed *= 0x5851F42D4C957F2D;
+        seed ^= hasher(v);
+        seed += 0x14057B7EF767814F;
+    }
+
+    template<class T>
+    static inline void reverse(std::size_t &seed, const T &v)
+    {
+        std::hash<T> hasher;
+        seed -= 0x14057B7EF767814F;
+        seed ^= hasher(v);
+        seed *= 0xC097EF87329E28A5; // 0x5851F42D4C957F2D^-1 MOD 2^64
     }
 
 }
