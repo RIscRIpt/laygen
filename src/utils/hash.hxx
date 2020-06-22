cfce039aa36899810f68414b2fceafa99d2adc8a
RIscRIpt <richard@riscript.com>
2020-06-22 14:32:50 +0300

    Add utils::hash::mix

--- src/utils/hash.hxx
+++ src/utils/hash.hxx
@@ -4,8 +4,10 @@
 
 namespace rstc::utils::hash {
 
+    // Order-dependent combination
+    // Use `reverse` to un-hash `v`.
     template<class T>
-    static inline void combine(std::size_t &seed, const T &v)
+    inline void combine(std::size_t &seed, const T &v)
     {
         std::hash<T> hasher;
         seed *= 0x5851F42D4C957F2D;
@@ -14,12 +16,21 @@ namespace rstc::utils::hash {
     }
 
     template<class T>
-    static inline void reverse(std::size_t &seed, const T &v)
+    inline void reverse(std::size_t &seed, const T &v)
     {
         std::hash<T> hasher;
         seed -= 0x14057B7EF767814F;
         seed ^= hasher(v);
         seed *= 0xC097EF87329E28A5; // 0x5851F42D4C957F2D^-1 MOD 2^64
+    }
+
+    // Order-independent combination
+    // Use `mix` to hash and un-hash `v`.
+    template<class T>
+    inline void mix(std::size_t &seed, const T &v)
+    {
+        std::hash<T> hasher;
+        seed ^= hasher(v) * 0x5851F42D4C957F2D + 0x14057B7EF767814F;
     }
 
 }
